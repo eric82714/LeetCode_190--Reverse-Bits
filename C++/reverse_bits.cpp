2f6956c07a4f98779a21c6cc04ad54ed9f9f730a
eric82714 <0958820714ab@gmail.com>
2020-01-30 11:46:35 +0800

    Create reverse_bits.cpp

---
+++ C++/reverse_bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t mod = 0, result = 0, counter = 0;
+        
+        while(n || counter < 32){
+            mod = n % 2;
+            n /= 2;
+            result = (result * 2) + mod;
+            counter++;
+        }
+        
+        return result;
+    }
+};
